8720a72655fb5263e561f54084df9323012c63a2
XHfight <X_H_fight@163.com>
2017-07-07 14:30:25 +0800

    select_server

--- IO复用/select/select_server.c
+++ IO复用/select/select_server.c
@@ -8,9 +8,9 @@
 #include <unistd.h>
 #include <sys/select.h>
 
-#define _SIZE_ 10
+#define _SIZE_ 1024 //sizeof(fd_set)*8
 
-int fd_arr[_SIZE_];
+int fd_arr[_SIZE_];  //保存文件被描述符的数组
 int max_fd = 0;
 
 static void Init_fd_arr()
@@ -50,6 +50,7 @@ static int Remove_fd_arr(int fd)
 	return 1;
 }
 
+//将文件描述符重新设置进fd_set中并获取文件描述符的最大值
 static int Reload_fd_set(fd_set *set)
 {
 	assert(set);
@@ -78,6 +79,9 @@ int Startup(const char* ip, int port)
 		exit(2);
 	}
 
+	//客户端连着我，服务器端主动断开连接。
+	//四次挥手服务器处于timewait时间,服务器无法重启。这个连接没有被释放，ip地址和端口号仍然被占用。
+	//解决：复用端口号。保证服务器关掉后可以立即重启。
 	int opt = 1;
 	setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
@@ -117,7 +121,7 @@ int main(int argc, char *argv[])
 	while(1)
 	{
 		max_fd = Reload_fd_set(&rfds);
-		struct timeval timeout = {5, 0};
+		struct timeval timeout = {5, 0};//等待5秒
 
 		switch(select(max_fd+1, &rfds, NULL, NULL, &timeout))
 		{
@@ -128,7 +132,7 @@ int main(int argc, char *argv[])
 			perror("select");
 			break;
 		default:
-			{
+			{//至少有一个文件描述符就绪
 				int index = 0;
 				for( ; index < _SIZE_; ++index)
 				{
@@ -136,6 +140,7 @@ int main(int argc, char *argv[])
 					if(index == 0 && fd_arr[index] != -1 \
 							&& FD_ISSET(listen_sock, &rfds))
 					{
+						//listen_sock已经就绪
 						struct sockaddr_in peer;
 						socklen_t size = sizeof(peer);
 						int conn_sock = accept(listen_sock, \
@@ -149,10 +154,16 @@ int main(int argc, char *argv[])
 								close(conn_sock);
 							}
 						}
+						else
+						{
+							perror("accept");
+							continue;
+						}
 					}
 					else if(fd_arr[index] != -1 && FD_ISSET(fd_arr[index], &rfds))
 					{	
 						//处理读事件
+						//read有问题，这里先假定读的数据小于缓冲区大小。
 						char buf[1024];
 						ssize_t s = read(fd_arr[index], buf, sizeof(buf)-1);
 						if(s > 0)
